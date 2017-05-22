0917dec27f2db69e6eb13ccb3376c1807e44593b
Alvaro <alvarodrx@gmail.com>
2017-05-22 10:32:06 -0600

    Proyecto netbeans en blanco
    Primera confirmación

---
+++ Proyecto_Demo/src/proyecto_demo/Proyecto_Demo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_demo;
+
+/**
+ *
+ * @author alvar_000
+ */
+public class Proyecto_Demo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
